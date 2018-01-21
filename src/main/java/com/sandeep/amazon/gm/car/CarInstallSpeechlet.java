ec06522aed1ebb61041840502c6375f6224470d9
Sandeep Shabd <sandeep.shabd@gm.com>
2018-01-21 08:13:17 -0500

    code to install app interactions

--- src/main/java/com/sandeep/amazon/gm/car/CarInstallSpeechlet.java
+++ src/main/java/com/sandeep/amazon/gm/car/CarInstallSpeechlet.java
@@ -1,13 +1,17 @@
 package com.sandeep.amazon.gm.car;
 
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.slu.Intent;
+import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
 import com.amazon.speech.speechlet.SessionEndedRequest;
 import com.amazon.speech.speechlet.SessionStartedRequest;
 import com.amazon.speech.speechlet.SpeechletResponse;
@@ -17,18 +21,21 @@ import com.amazon.speech.ui.Reprompt;
 
 public class CarInstallSpeechlet  implements SpeechletV2{
 	private static final Logger log = LoggerFactory.getLogger(CarInstallSpeechlet.class);
+	private static final String APPS_SLOT = "Application";
+	private static final String APP_KEY = "APP";
 
 	@Override
 	public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
 		log.info("onIntent");
 		IntentRequest request = requestEnvelope.getRequest();
+		Session session = requestEnvelope.getSession();
         log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
-                requestEnvelope.getSession().getSessionId());
+        		session.getSessionId());
 
         Intent intent = request.getIntent();
         String intentName = (intent != null) ? intent.getName() : null;
         if ("InstallAppIntent".equals(intentName)) {
-            return installApp(intent);
+            return installApp(intent, session);
         }else if ("WhatsMyAppStatusIntent".equals(intentName)) {
             return getAppStatus(intent);
         } else if ("AMAZON.HelpIntent".equals(intentName)) {
@@ -49,8 +56,32 @@ public class CarInstallSpeechlet  implements SpeechletV2{
         }
 	}
 
-	private SpeechletResponse installApp(Intent intent) {
+	private SpeechletResponse installApp(Intent intent, Session session) {
 		// TODO Auto-generated method stub
+		Map<String, Slot> slots = intent.getSlots();
+		String speechText, repromptText;
+        // Get the color slot from the list of slots.
+        Slot appToInstallSlot = slots.get(APPS_SLOT);
+        if (appToInstallSlot != null) {
+            // Store the user's favorite color in the Session and create response.
+        	 	
+            String appToInstall = appToInstallSlot.getValue();
+            session.setAttribute(APP_KEY, appToInstall);
+            speechText =
+                    String.format("Java, I am going to install %s. You can ask me the status of the app by saying,  "
+                            + " what is the status?", appToInstall);
+            repromptText =
+                    "You can ask me the status of the app by saying, what is the status?";
+
+        } else {
+            // Render an error since we don't know what the users favorite color is.
+            speechText = "I'm not sure what your app is, please try again";
+            repromptText =
+                    "I'm not sure whatyour app is. You can ask me to install application like,  "
+                            + "install Audiobooks, or, you can say exit..."
+                            + "Now, what can I help you with?";
+        }
+
 		return null;
 	}
 
@@ -81,7 +112,7 @@ public class CarInstallSpeechlet  implements SpeechletV2{
 	
     private SpeechletResponse getHelp() {
         String speechOutput =
-                "You can ask me to install application like  "
+                "Java, You can ask me to install application like  "
                         + "install Audiobooks, or, you can say exit... "
                         + "Now, what can I help you with?";
         String repromptText =
